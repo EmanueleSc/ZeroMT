93b5d310208e07ba0f4465a25ab10095d4a8699e
Francesco Pio Stelluti <34342660+FrancisFire@users.noreply.github.com>
2022-06-02 12:56:13 +0200

    [UPDATE] Refactored bit conversion and aL, aR generation

--- src/proof_system_utils.rs
+++ src/proof_system_utils.rs
@@ -6,9 +6,8 @@ use ark_crypto_primitives::commitment::pedersen::Window;
 use ark_crypto_primitives::crh::pedersen::CRH;
 use ark_ec::short_weierstrass_jacobian::GroupAffine;
 use ark_ec::{AffineCurve, ProjectiveCurve};
-use ark_ff::{PrimeField, Zero};
+use ark_ff::{One, PrimeField, Zero};
 use ark_std::UniformRand;
-use bitreader::BitReader;
 use rand::Rng;
 
 #[derive(Clone)]
@@ -52,6 +51,21 @@ impl ProofSystemUtils {
         return scalars;
     }
 
+    pub fn get_n_random_scalars_not_zero<R: Rng>(
+        number_of_points: usize,
+        rng: &mut R,
+    ) -> Vec<ScalarField> {
+        let mut scalars = Vec::<ScalarField>::with_capacity(number_of_points);
+        for _ in 0..number_of_points {
+            let mut to_push: ScalarField = ScalarField::rand(rng);
+            while to_push == ScalarField::zero() {
+                to_push = ScalarField::rand(rng);
+            }
+            scalars.push(to_push);
+        }
+        return scalars;
+    }
+
     pub fn get_curve_generator() -> GroupAffine<G1Parameters> {
         return G1Affine::new(G1_GENERATOR_X, G1_GENERATOR_Y, false);
     }
@@ -75,26 +89,6 @@ impl ProofSystemUtils {
         return Ok(result);
     }
 
-    pub fn bit_inner_product(
-        points: &Vec<GroupAffine<G1Parameters>>,
-        bits: &Vec<i8>,
-    ) -> Result<GroupAffine<G1Parameters>, &'static str> {
-        if points.len() != bits.len() {
-            return Err("Different lengths! Error!");
-        } else {
-            let result = points
-                .iter()
-                .zip(bits.iter())
-                .map(|(p, b)| {
-                    let to_return = p.mul(b.abs() as u64).into_affine();
-                    return if *b < 0 { -to_return } else { to_return };
-                })
-                .sum();
-
-            return Ok(result);
-        }
-    }
-
     // b_scalar * b_point + <g_scalar_vec, g_point_vec> + <h_scalar_vec, h_point_vec>
     pub fn pedersen_vector_commitment(
         b_scalar: &ScalarField,
@@ -115,60 +109,6 @@ impl ProofSystemUtils {
         }
     }
 
-    // b_scalar * b_point + <g_bit_vec, g_point_vec> + <h_bit_vec, h_point_vec>
-    pub fn bit_pedersen_vector_commitment(
-        b_scalar: &ScalarField,
-        b_point: &GroupAffine<G1Parameters>,
-        g_bit_vec: &Vec<i8>,
-        g_point_vec: &Vec<GroupAffine<G1Parameters>>,
-        h_bit_vec: &Vec<i8>,
-        h_point_vec: &Vec<GroupAffine<G1Parameters>>,
-    ) -> Result<GroupAffine<G1Parameters>, &'static str> {
-        let first_inner_product = Self::bit_inner_product(g_point_vec, g_bit_vec);
-        let second_inner_product = Self::bit_inner_product(h_point_vec, h_bit_vec);
-        if (first_inner_product.is_err() || second_inner_product.is_err()) {
-            return Err("Inner product error!");
-        } else {
-            return Ok(b_point.mul(b_scalar.into_repr()).into_affine()
-                + first_inner_product.unwrap()
-                + second_inner_product.unwrap());
-        }
-    }
-
-    pub fn pedersen_test(balance: usize, amounts: &Vec<usize>) {
-        let mut rng = ark_std::rand::thread_rng();
-
-        let alpha = Self::get_n_random_scalars(1, &mut rng)[0];
-        let rho = Self::get_n_random_scalars(1, &mut rng)[0];
-
-        let a_L = Self::get_a_L(balance, amounts);
-        let a_R = Self::get_a_R(&a_L);
-
-        let s_L = Self::get_n_random_scalars(Self::get_n_by_m(amounts.len() + 1), &mut rng);
-        let s_R = Self::get_n_random_scalars(Self::get_n_by_m(amounts.len() + 1), &mut rng);
-
-        let g_vec = Self::get_n_generators_berkeley(Self::get_n_by_m(amounts.len() + 1), &mut rng);
-        let h_vec = Self::get_n_generators_berkeley(Self::get_n_by_m(amounts.len() + 1), &mut rng);
-
-        let h = Self::get_n_random_points(1, &mut rng)[0];
-
-        let A_commitment =
-            Self::bit_pedersen_vector_commitment(&alpha, &h, &a_L, &g_vec, &a_R, &h_vec);
-
-        let S_commitment = Self::pedersen_vector_commitment(&rho, &h, &s_L, &g_vec, &s_R, &h_vec);
-
-        println!(
-            "A commitment {:?} - on curve {}",
-            A_commitment,
-            A_commitment.unwrap().is_on_curve()
-        );
-        println!(
-            "S commitment {:?} - on curve {}",
-            S_commitment,
-            S_commitment.unwrap().is_on_curve()
-        );
-    }
-
     pub fn get_n_by_m(m: usize) -> usize {
         return (usize::BITS as usize) * m;
     }
@@ -177,24 +117,21 @@ impl ProofSystemUtils {
         return usize::BITS as usize;
     }
 
-    pub fn number_to_be_bits(number: usize) -> Vec<i8> {
-        let mut bits = Vec::<i8>::with_capacity(Self::get_n());
-        let bytes = number.to_be_bytes();
-        let mut reader = BitReader::new(&bytes);
-        while reader.remaining() > 0 {
-            bits.push(if reader.read_bool().unwrap() { 1 } else { 0 });
-        }
-        return bits;
-    }
-
-    pub fn number_to_be_bits_reversed(number: usize) -> Vec<i8> {
-        let mut bits = Self::number_to_be_bits(number);
+    pub fn number_to_be_bits(number: usize) -> Vec<u8> {
+        let mut bits: Vec<u8> = Self::number_to_be_bits_reversed(number);
         bits.reverse();
         return bits;
     }
 
-    pub fn get_a_L(balance: usize, amounts: &Vec<usize>) -> Vec<i8> {
-        let mut bits = Vec::<i8>::with_capacity(Self::get_n_by_m(amounts.len() + 1));
+    pub fn number_to_be_bits_reversed(number: usize) -> Vec<u8> {
+        let mut bits: Vec<u8> = (0..Self::get_n())
+            .map(|i| (((number >> i) & 1) as u8))
+            .collect();
+        return bits;
+    }
+
+    pub fn get_a_L(balance: usize, amounts: &Vec<usize>) -> Vec<ScalarField> {
+        let mut bits = Vec::<u8>::with_capacity(Self::get_n_by_m(amounts.len() + 1));
         Self::number_to_be_bits_reversed(balance)
             .iter()
             .for_each(|bit| bits.push(*bit));
@@ -206,30 +143,30 @@ impl ProofSystemUtils {
                 bit_array.iter().for_each(|bit| bits.push(*bit));
             });
 
-        return bits;
+        return bits.iter().map(|bit| ScalarField::from(*bit)).collect();
     }
 
-    pub fn get_a_R(a_L: &Vec<i8>) -> Vec<i8> {
-        return a_L.iter().map(|bit| bit - 1).collect();
+    pub fn get_a_R(a_L: &Vec<ScalarField>) -> Vec<ScalarField> {
+        return a_L.iter().map(|bit| *bit - ScalarField::one()).collect();
     }
 
     pub fn test_number_to_be_bits() {
         let test_number: usize = 42;
-        let mut test_number_bits: Vec<i8> = [
+        let mut test_number_bits: Vec<u8> = [
             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
             1, 0, 1, 0, 1, 0,
         ]
         .to_vec();
 
-        let resulting_number_bits: Vec<i8> = Self::number_to_be_bits(test_number);
+        let resulting_number_bits: Vec<u8> = Self::number_to_be_bits(test_number);
 
         assert_eq!(test_number_bits, resulting_number_bits);
     }
 
     pub fn test_number_to_be_bits_reversed() {
         let test_number: usize = 42;
-        let mut test_number_bits: Vec<i8> = [
+        let mut test_number_bits: Vec<u8> = [
             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
             1, 0, 1, 0, 1, 0,
@@ -237,7 +174,7 @@ impl ProofSystemUtils {
         .to_vec();
 
         test_number_bits.reverse();
-        let resulting_number_bits: Vec<i8> = Self::number_to_be_bits_reversed(test_number);
+        let resulting_number_bits: Vec<u8> = Self::number_to_be_bits_reversed(test_number);
 
         assert_eq!(test_number_bits, resulting_number_bits);
     }
@@ -296,94 +233,150 @@ impl ProofSystemUtils {
         test_a_L.extend(four_bits);
         test_a_L.extend(five_bits);
 
-        let result_a_L: Vec<i8> = Self::get_a_L(test_balance, &test_amounts);
+        let test_a_L_scalar: Vec<ScalarField> =
+            test_a_L.iter().map(|bit| ScalarField::from(*bit)).collect();
+        let result_a_L: Vec<ScalarField> = Self::get_a_L(test_balance, &test_amounts);
 
-        assert_eq!(result_a_L, test_a_L);
+        assert_eq!(result_a_L, test_a_L_scalar);
     }
 
     pub fn test_get_a_R() {
-        let test_a_L: Vec<i8> = [1, 0, 1, 1, 0, 0, 0, 0, 1].to_vec();
-        let test_a_R: Vec<i8> = [0, -1, 0, 0, -1, -1, -1, -1, 0].to_vec();
-        let result_a_R: Vec<i8> = Self::get_a_R(&test_a_L);
+        let test_a_L_scalar: Vec<ScalarField> = [1, 0, 1, 1, 0, 0, 0, 0, 1]
+            .to_vec()
+            .iter()
+            .map(|bit| ScalarField::from(*bit))
+            .collect();
 
-        assert_eq!(result_a_R, test_a_R);
+        let test_a_R_scalar: Vec<ScalarField> = [0, -1, 0, 0, -1, -1, -1, -1, 0]
+            .to_vec()
+            .iter()
+            .map(|bit| ScalarField::from(*bit))
+            .collect();
+
+        let result_a_R: Vec<ScalarField> = Self::get_a_R(&test_a_L_scalar);
+
+        assert_eq!(result_a_R, test_a_R_scalar);
     }
 
-    pub fn test_bit_inner_product() {
+    pub fn test_inner_product() {
         let mut rng = ark_std::rand::thread_rng();
         let test_points: Vec<GroupAffine<G1Parameters>> =
             Self::get_n_generators_berkeley(2, &mut rng);
 
-        let test_bits_one: Vec<i8> = [1, 1].to_vec();
-        let test_bits_two: Vec<i8> = [1, 0].to_vec();
-        let test_bits_three: Vec<i8> = [0, 1].to_vec();
-        let test_bits_four: Vec<i8> = [0, 0].to_vec();
+        let test_bits_one: Vec<ScalarField> = [1, 1]
+            .to_vec()
+            .iter()
+            .map(|bit| ScalarField::from(*bit))
+            .collect();
+        let test_bits_two: Vec<ScalarField> = [1, 0]
+            .to_vec()
+            .iter()
+            .map(|bit| ScalarField::from(*bit))
+            .collect();
+        let test_bits_three: Vec<ScalarField> = [0, 1]
+            .to_vec()
+            .iter()
+            .map(|bit| ScalarField::from(*bit))
+            .collect();
+        let test_bits_four: Vec<ScalarField> = [0, 0]
+            .to_vec()
+            .iter()
+            .map(|bit| ScalarField::from(*bit))
+            .collect();
 
-        let test_bits_minus_one: Vec<i8> = [-1, -1].to_vec();
-        let test_bits_minus_two: Vec<i8> = [-1, 0].to_vec();
-        let test_bits_minus_three: Vec<i8> = [0, -1].to_vec();
+        let test_bits_minus_one: Vec<ScalarField> = [-1, -1]
+            .to_vec()
+            .iter()
+            .map(|bit| ScalarField::from(*bit))
+            .collect();
+        let test_bits_minus_two: Vec<ScalarField> = [-1, 0]
+            .to_vec()
+            .iter()
+            .map(|bit| ScalarField::from(*bit))
+            .collect();
+        let test_bits_minus_three: Vec<ScalarField> = [0, -1]
+            .to_vec()
+            .iter()
+            .map(|bit| ScalarField::from(*bit))
+            .collect();
 
-        let test_bits_mix_one: Vec<i8> = [1, -1].to_vec();
-        let test_bits_mix_two: Vec<i8> = [-1, 1].to_vec();
+        let test_bits_mix_one: Vec<ScalarField> = [1, -1]
+            .to_vec()
+            .iter()
+            .map(|bit| ScalarField::from(*bit))
+            .collect();
+        let test_bits_mix_two: Vec<ScalarField> = [-1, 1]
+            .to_vec()
+            .iter()
+            .map(|bit| ScalarField::from(*bit))
+            .collect();
 
         let result_inner_product_one: GroupAffine<G1Parameters> =
-            Self::bit_inner_product(&test_points, &test_bits_one).unwrap();
+            Self::inner_product(&test_points, &test_bits_one).unwrap();
         assert_eq!(result_inner_product_one, test_points[0] + test_points[1]);
 
         let result_inner_product_two: GroupAffine<G1Parameters> =
-            Self::bit_inner_product(&test_points, &test_bits_two).unwrap();
+            Self::inner_product(&test_points, &test_bits_two).unwrap();
         assert_eq!(
             result_inner_product_two,
             test_points[0] + GroupAffine::zero()
         );
 
         let result_inner_product_three: GroupAffine<G1Parameters> =
-            Self::bit_inner_product(&test_points, &test_bits_three).unwrap();
+            Self::inner_product(&test_points, &test_bits_three).unwrap();
         assert_eq!(
             result_inner_product_three,
             GroupAffine::zero() + test_points[1]
         );
 
         let result_inner_product_four: GroupAffine<G1Parameters> =
-            Self::bit_inner_product(&test_points, &test_bits_four).unwrap();
+            Self::inner_product(&test_points, &test_bits_four).unwrap();
         assert_eq!(
             result_inner_product_four,
             GroupAffine::zero() + GroupAffine::zero()
         );
 
         let result_inner_product_minus_one: GroupAffine<G1Parameters> =
-            Self::bit_inner_product(&test_points, &test_bits_minus_one).unwrap();
+            Self::inner_product(&test_points, &test_bits_minus_one).unwrap();
         assert_eq!(
             result_inner_product_minus_one,
             -test_points[0] + -test_points[1]
         );
 
         let result_inner_product_minus_two: GroupAffine<G1Parameters> =
-            Self::bit_inner_product(&test_points, &test_bits_minus_two).unwrap();
+            Self::inner_product(&test_points, &test_bits_minus_two).unwrap();
         assert_eq!(
             result_inner_product_minus_two,
             -test_points[0] + GroupAffine::zero()
         );
 
         let result_inner_product_minus_three: GroupAffine<G1Parameters> =
-            Self::bit_inner_product(&test_points, &test_bits_minus_three).unwrap();
+            Self::inner_product(&test_points, &test_bits_minus_three).unwrap();
         assert_eq!(
             result_inner_product_minus_three,
             GroupAffine::zero() + -test_points[1]
         );
 
         let result_inner_product_mix_one: GroupAffine<G1Parameters> =
-            Self::bit_inner_product(&test_points, &test_bits_mix_one).unwrap();
+            Self::inner_product(&test_points, &test_bits_mix_one).unwrap();
         assert_eq!(
             result_inner_product_mix_one,
             test_points[0] + -test_points[1]
         );
 
         let result_inner_product_mix_two: GroupAffine<G1Parameters> =
-            Self::bit_inner_product(&test_points, &test_bits_mix_two).unwrap();
+            Self::inner_product(&test_points, &test_bits_mix_two).unwrap();
         assert_eq!(
             result_inner_product_mix_two,
             -test_points[0] + test_points[1]
         );
+    }
+
+    pub fn l_poly(a_L: &Vec<i8>, z: ScalarField, s_L: Vec<ScalarField>) {
+        if (a_L.len() != s_L.len()) {
+            todo!()
+        }
+
+        // a_L.iter().map(|bit| *bit - z.into_repr())
     }
 }
