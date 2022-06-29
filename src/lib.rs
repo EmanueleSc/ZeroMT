2b2fbaa2c112418f28b07bab9d92d3f626bac5b8
Francesco Pio Stelluti <34342660+FrancisFire@users.noreply.github.com>
2022-06-29 10:32:23 +0200

    [ADD] First benchmark trait implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![feature(int_log)]
 
+mod benchmark;
 mod errors;
 mod inner;
 mod range;
@@ -10,34 +11,36 @@ mod sigma_y;
 mod transcript;
 mod utils;
 
+pub use crate::benchmark::exec_time_benchmark::ExecTimeBenchmark;
+
 pub use crate::errors::proof_error::ProofError;
 pub use crate::errors::transcript_error::TranscriptError;
 pub use crate::errors::utils_error::UtilsError;
 
-pub use crate::inner::inner_proof::Proof as InnerProof;
-pub use crate::inner::inner_prover::Prover as InnerProver;
-pub use crate::inner::inner_verifier::Verifier as InnerVerifier;
+pub use crate::inner::inner_proof::InnerProof;
+pub use crate::inner::inner_prover::InnerProver;
+pub use crate::inner::inner_verifier::InnerVerifier;
 
 pub use crate::range::inner_proof_arguments::InnerProofArguments;
-pub use crate::range::range_proof::Proof as RangeProof;
-pub use crate::range::range_prover::Prover as RangeProver;
-pub use crate::range::range_verifier::Verifier as RangeVerifier;
+pub use crate::range::range_proof::RangeProof;
+pub use crate::range::range_prover::RangeProver;
+pub use crate::range::range_verifier::RangeVerifier;
 
-pub use crate::sigma_r::sigma_r_proof::Proof as SigmaRProof;
-pub use crate::sigma_r::sigma_r_prover::Prover as SigmaRProver;
-pub use crate::sigma_r::sigma_r_verifier::Verifier as SigmaRVerifier;
+pub use crate::sigma_r::sigma_r_proof::SigmaRProof;
+pub use crate::sigma_r::sigma_r_prover::SigmaRProver;
+pub use crate::sigma_r::sigma_r_verifier::SigmaRVerifier;
 
-pub use crate::sigma_sk::sigma_sk_proof::Proof as SigmaSkProof;
-pub use crate::sigma_sk::sigma_sk_prover::Prover as SigmaSkProver;
-pub use crate::sigma_sk::sigma_sk_verifier::Verifier as SigmaSkVerifier;
+pub use crate::sigma_sk::sigma_sk_proof::SigmaSKProof;
+pub use crate::sigma_sk::sigma_sk_prover::SigmaSKProver;
+pub use crate::sigma_sk::sigma_sk_verifier::SigmaSKVerifier;
 
-pub use crate::sigma_y::sigma_y_proof::Proof as SigmaYProof;
-pub use crate::sigma_y::sigma_y_prover::Prover as SigmaYProver;
-pub use crate::sigma_y::sigma_y_verifier::Verifier as SigmaYVerifier;
+pub use crate::sigma_y::sigma_y_proof::SigmaYProof;
+pub use crate::sigma_y::sigma_y_prover::SigmaYProver;
+pub use crate::sigma_y::sigma_y_verifier::SigmaYVerifier;
 
-pub use crate::sigma_ab::sigma_ab_proof::Proof as SigmaABProof;
-pub use crate::sigma_ab::sigma_ab_prover::Prover as SigmaABProver;
-pub use crate::sigma_ab::sigma_ab_verifier::Verifier as SigmaABVerifier;
+pub use crate::sigma_ab::sigma_ab_proof::SigmaABProof;
+pub use crate::sigma_ab::sigma_ab_prover::SigmaABProver;
+pub use crate::sigma_ab::sigma_ab_verifier::SigmaABVerifier;
 
 pub use crate::transcript::TranscriptProtocol;
 pub use crate::utils::Utils;
